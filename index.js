96264ff526ba181dc06a9132724517ee03746622
Jonathan Apodaca <jrapodaca@gmail.com>
2018-04-27 14:23:13 -0600

    Code quality:
    * 100% coverage
    * Switch to NYC
    * Husky (precommit)
    * ESLint
    * Prettier
    * Mocha => Tape

--- index.js
+++ index.js
@@ -3,28 +3,12 @@
 const DAY_MS = 86400000
 
 //
-// Calculate the first and last days of a given month
-//
-function range(month, year) {
-	const nextMonth = (month + 1) % 12
-	const nextYear = month + 1 > 11 ? year + 1 : year
-
-	const first = new Date(year, month, 1)
-	const lastMs = new Date(nextYear, nextMonth, 1).getTime() - DAY_MS
-	const last = new Date(lastMs)
-
-	return { first, last }
-}
-
-//
 // Returns an array of weeks
 //
 function month(month, year) {
 	year = year || new Date().getFullYear()
-	const { first, last } = range(month, year)
-	const info = new Array(6)
-		.fill(null)
-		.map(() => new Array(7).fill(null))
+	const first = new Date(year, month, 1)
+	const info = new Array(6).fill(null).map(() => new Array(7).fill(null))
 
 	const firstDayOnGrid = first.getTime() - first.getDay() * DAY_MS
 	for (let w = 0; w < 6; ++w) {
