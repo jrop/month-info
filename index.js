ea0aff6bc0108a59940639a5a60adc1d807dfe2b
Jonathan Apodaca <jrapodaca@gmail.com>
2024-08-07 20:53:54 -0600

    (chore) modernize
    use JSDoc for now. I might switch this to *.ts, but for now this is fine

--- index.js
+++ index.js
@@ -1,25 +1,36 @@
-'use strict'
+// @ts-check
+const DAY_MS = 86400000;
+const NAN = Number.NaN;
 
-const DAY_MS = 86400000
-
-//
-// Returns an array of weeks
-//
+/**
+ * Returns an array of weeks
+ * @param {number} month - 0-based
+ * @param {number=} year - defaults to current year
+ * @returns {number[][]} - 6x7 array
+ */
 function month(month, year) {
-	year = year || new Date().getFullYear()
-	const first = new Date(year, month, 1)
-	const info = new Array(6).fill(null).map(() => new Array(7).fill(null))
+  // biome-ignore lint/style/noParameterAssign:
+  year = year !== undefined ? year : new Date().getFullYear();
+  const first = new Date(year, month, 1);
+  const info = [
+    [NAN, NAN, NAN, NAN, NAN, NAN, NAN],
+    [NAN, NAN, NAN, NAN, NAN, NAN, NAN],
+    [NAN, NAN, NAN, NAN, NAN, NAN, NAN],
+    [NAN, NAN, NAN, NAN, NAN, NAN, NAN],
+    [NAN, NAN, NAN, NAN, NAN, NAN, NAN],
+    [NAN, NAN, NAN, NAN, NAN, NAN, NAN],
+  ];
 
-	const firstDayOnGrid = first.getTime() - first.getDay() * DAY_MS
-	for (let w = 0; w < 6; ++w) {
-		for (let d = 0; d < 7; ++d) {
-			const dayOnGrid = (w * 7 + d) * DAY_MS
-			const curr = new Date(firstDayOnGrid + dayOnGrid)
-			const currDay = curr.getUTCDate()
-			info[w][d] = curr.getUTCMonth() == month ? currDay : -currDay
-		}
-	}
-	return info
+  const firstDayOnGrid = first.getTime() - first.getDay() * DAY_MS;
+  for (let w = 0; w < 6; ++w) {
+    for (let d = 0; d < 7; ++d) {
+      const dayOnGrid = (w * 7 + d) * DAY_MS;
+      const curr = new Date(firstDayOnGrid + dayOnGrid);
+      const currDay = curr.getUTCDate();
+      info[w][d] = curr.getUTCMonth() === month ? currDay : -currDay;
+    }
+  }
+  return info;
 }
 
-module.exports = month
+export default month;
